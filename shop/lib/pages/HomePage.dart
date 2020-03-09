4fdf081b1330f5c883a0b95ace9039927f7c56fa
YangMr <759168442@qq.com>
2020-03-09 10:52:51 +0800

    04 制作轮播图

--- shop/lib/pages/HomePage.dart
+++ shop/lib/pages/HomePage.dart
@@ -1,6 +1,9 @@
 //引入ui库
 import 'package:flutter/material.dart';
 
+//引入轮播图插件
+import 'package:flutter_swiper/flutter_swiper.dart';
+
 //创建组件
 class HomePage extends StatefulWidget {
   HomePage({Key key}) : super(key: key);
@@ -10,10 +13,40 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
+  //轮播图组件
+  Widget _swiperWidget() {
+    //模拟轮播图数据
+    List<Map> swiperUrl = [
+      {"url": "https://www.itying.com/images/flutter/slide01.jpg"},
+      {"url": "https://www.itying.com/images/flutter/slide02.jpg"},
+      {"url": "https://www.itying.com/images/flutter/slide03.jpg"},
+    ];
+    return Container(
+      child: AspectRatio(
+        aspectRatio: 2 / 1,
+        child: new Swiper(
+          itemBuilder: (BuildContext context, int index) {
+            return new Image.network(
+              swiperUrl[index]['url'],
+              fit: BoxFit.fill,
+            );
+          },
+          itemCount: swiperUrl.length,
+          autoplay: true,
+          pagination: new SwiperPagination(),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-       child: Text("首页"),
+    return ListView(
+      children: <Widget>[
+        //轮播图
+        _swiperWidget()
+      ],
     );
   }
 }
