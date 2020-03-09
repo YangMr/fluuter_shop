32860b48219ca8ff3e5a3b5019265fdb138a7397
YangMr <759168442@qq.com>
2020-03-09 11:22:29 +0800

    05 制作左右滑动的商品列表

--- shop/lib/pages/HomePage.dart
+++ shop/lib/pages/HomePage.dart
@@ -13,7 +13,6 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-
   //轮播图组件
   Widget _swiperWidget() {
     //模拟轮播图数据
@@ -29,7 +28,7 @@ class _HomePageState extends State<HomePage> {
           itemBuilder: (BuildContext context, int index) {
             return new Image.network(
               swiperUrl[index]['url'],
-              fit: BoxFit.fill,
+              fit: BoxFit.cover,
             );
           },
           itemCount: swiperUrl.length,
@@ -40,12 +39,79 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
+  //封装标题组件
+  Widget _titleWidget(title) {
+    return Container(
+      margin: EdgeInsets.all(10),
+      padding: EdgeInsets.only(left: 10),
+      child: Text(
+        title,
+        style: TextStyle(fontWeight: FontWeight.w500),
+      ),
+      decoration: BoxDecoration(
+        border: Border(
+          left: BorderSide(color: Colors.red, width: 6),
+        ),
+      ),
+    );
+  }
+
+  //封装猜你喜欢列表
+  Widget _loveItem() {
+    return Column(
+      children: <Widget>[
+        Container(
+          margin: EdgeInsets.only(left: 10),
+          width: 80,
+          height: 80,
+          child: Image.network("https://www.itying.com/images/flutter/hot1.jpg",
+              fit: BoxFit.cover),
+        ),
+        Text("第0条")
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return ListView(
       children: <Widget>[
         //轮播图
-        _swiperWidget()
+        _swiperWidget(),
+
+        //定义猜你喜欢标题
+        _titleWidget("猜你喜欢"),
+
+        //定义左右滑动的推荐商品列表
+        Container(
+          height: 100,
+          width: double.infinity,
+          child: ListView(
+            scrollDirection: Axis.horizontal,
+            children: <Widget>[
+              _loveItem(),
+              _loveItem(),
+              _loveItem(),
+              _loveItem(),
+              _loveItem(),
+              _loveItem(),
+              _loveItem(),
+              _loveItem(),
+              _loveItem(),
+              _loveItem(),
+              _loveItem(),
+              _loveItem(),
+              _loveItem(),
+              _loveItem(),
+              _loveItem(),
+              _loveItem(),
+
+            ],
+          ),
+        ),
+
+        //定义热门推荐标题
+        _titleWidget("热门推荐"),
       ],
     );
   }
