bcc234a16c1de85da7113d206728ec65f028e36b
YangMr <759168442@qq.com>
2020-03-10 08:53:22 +0800

    06 封装适配库

--- shop/lib/pages/HomePage.dart
+++ shop/lib/pages/HomePage.dart
@@ -4,6 +4,9 @@ import 'package:flutter/material.dart';
 //引入轮播图插件
 import 'package:flutter_swiper/flutter_swiper.dart';
 
+//引入封装的ui插件库
+import '../utils/screen.dart';
+
 //创建组件
 class HomePage extends StatefulWidget {
   HomePage({Key key}) : super(key: key);
@@ -39,38 +42,6 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
-  //封装标题组件
-  Widget _titleWidget(title) {
-    return Container(
-      margin: EdgeInsets.all(10),
-      padding: EdgeInsets.only(left: 10),
-      child: Text(
-        title,
-        style: TextStyle(fontWeight: FontWeight.w500),
-      ),
-      decoration: BoxDecoration(
-        border: Border(
-          left: BorderSide(color: Colors.red, width: 6),
-        ),
-      ),
-    );
-  }
-
-  //封装猜你喜欢列表
-  Widget _loveItem() {
-    return Column(
-      children: <Widget>[
-        Container(
-          margin: EdgeInsets.only(left: 10),
-          width: 80,
-          height: 80,
-          child: Image.network("https://www.itying.com/images/flutter/hot1.jpg",
-              fit: BoxFit.cover),
-        ),
-        Text("第0条")
-      ],
-    );
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -79,39 +50,6 @@ class _HomePageState extends State<HomePage> {
         //轮播图
         _swiperWidget(),
 
-        //定义猜你喜欢标题
-        _titleWidget("猜你喜欢"),
-
-        //定义左右滑动的推荐商品列表
-        Container(
-          height: 100,
-          width: double.infinity,
-          child: ListView(
-            scrollDirection: Axis.horizontal,
-            children: <Widget>[
-              _loveItem(),
-              _loveItem(),
-              _loveItem(),
-              _loveItem(),
-              _loveItem(),
-              _loveItem(),
-              _loveItem(),
-              _loveItem(),
-              _loveItem(),
-              _loveItem(),
-              _loveItem(),
-              _loveItem(),
-              _loveItem(),
-              _loveItem(),
-              _loveItem(),
-              _loveItem(),
-
-            ],
-          ),
-        ),
-
-        //定义热门推荐标题
-        _titleWidget("热门推荐"),
       ],
     );
   }
---
+++ shop/lib/utils/screen.dart
@@ -0,0 +1,26 @@
+//引入适配库插件
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+//设置初始化的方法
+setInit(context){
+  //设置字体大小根据系统的“字体大小”辅助选项来进行缩放,默认为false
+  ScreenUtil.init(context, width: 750, height: 1334, allowFontScaling: true);
+}
+
+//设置宽度的方法
+setHeight(value){
+  return ScreenUtil().setHeight(value);
+}
+//设置高度的方法
+setWidth(value){
+  return ScreenUtil().setHeight(value);
+}
+//获取设备的宽度
+getScreenWidth(){
+  return ScreenUtil.screenWidth;
+}
+//获取设备的高度
+getScreeHeight(){
+  return ScreenUtil.screenHeight;
+}
+
